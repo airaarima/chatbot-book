bc4d9f905b1c238a29209f50897eaedfd9f6069d
PatsFerrer <patriciatferreira1@hotmail.com>
2025-05-05 17:44:20 -0300

    refact: adiciona validação a mais no e-mail do zod

--- frontend/src/schemas/registerSchema.ts
+++ frontend/src/schemas/registerSchema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const registerSchema = z
   .object({
     name: z.string().min(1, "Nome é obrigatório"),
-    email: z.string().email("E-mail inválido"),
+    email: z.string().min(1, "E-mail é obrigatório").email("E-mail inválido"),
     password: z
       .string()
       .min(6, "A senha deve ter no mínimo 6 caracteres")
