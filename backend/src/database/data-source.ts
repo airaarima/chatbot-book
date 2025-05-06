9344fc8d4b6e9d0ac621b335d130b19f8214def2
airaarima <arimacoelhoaira@gmail.com>
2025-05-06 11:32:11 -0300

    feat: configura docker e conexão com o banco de dados

---
+++ backend/src/database/data-source.ts
@@ -0,0 +1,17 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import { User } from "../entities/user"
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5430,
+    username: "postgres",
+    password: "root",
+    database: "chatbotdb",
+    synchronize: true,
+    logging: false,
+    entities: [User],
+    migrations: ["./src/database/migration/*.ts"],
+    subscribers: [],
+})
