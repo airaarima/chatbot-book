6653065292d046835d0d36bf9a222bf1cf84cd9b
airaarima <arimacoelhoaira@gmail.com>
2025-05-06 15:17:10 -0300

    refactor: cria controller para enviar mensagens para a IA

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,23 +1,17 @@
 import express from "express";
-import { getGroqChatCompletion } from "./services/groqService";
 import dotenv from "dotenv";
 import cors from "cors";
+import groqController from "./controllers/groqController";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post("/api/message", async (req, res) => {
-  try {
-    const userMessage = req.body.pergunta;
-    const reply = await getGroqChatCompletion(userMessage);
-    res.json({ resposta: reply.choices[0]?.message?.content });
-  } catch (err: any) {
-    console.error("Error:", err);
-    res.status(500).json({ error: err.message });
-  }
-});
+const route = "/api/v1";
+
+app.post(`${route}/message`, groqController.sendMessage);
+
 
 const PORT = process.env.PORT;
 
