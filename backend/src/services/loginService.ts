2cc6ddd92bba9974f4d023e99fabc47c5cbd342a
Karina Santana <karina.santana@esx.com.br>
2025-05-14 16:28:18 -0300

    fix: ajuste padrao lint

--- backend/src/services/loginService.ts
+++ backend/src/services/loginService.ts
@@ -1,5 +1,3 @@
-import { sign } from "jsonwebtoken";
-
 import userRepository from "../repositories/userRepository";
 
 const loginUser = async (credentials: { email: string; password: string }) => {
@@ -15,12 +13,6 @@ const loginUser = async (credentials: { email: string; password: string }) => {
     return { status: 401, message: "Credenciais inválidas" };
   }
 
-  const token = sign(
-    { id: user.id, email: user.email },
-    process.env.JWT_SECRET || "your-secret-key",
-    { expiresIn: "1d" },
-  );
-
   // Remover a senha antes de retornar
   const { ...userWithoutPassword } = user;
 
@@ -29,7 +21,6 @@ const loginUser = async (credentials: { email: string; password: string }) => {
     message: "Login realizado com sucesso",
     data: {
       user: userWithoutPassword,
-      token,
     },
   };
 };
